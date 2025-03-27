c6e5b4a95eeb3162ba5c84d61d846bf5d1046669
sreiswig <sam.reiswig@gmail.com>
2025-03-27 00:51:55 -0500

    statement made

--- BerkeleyProblemsInMathematics/Basic.lean
+++ BerkeleyProblemsInMathematics/Basic.lean
@@ -1,1 +1,7 @@
-def hello := "world"
+import Mathlib.Analysis.Calculus.Deriv.Basic
+import Mathlib.Analysis.Calculus.Deriv.Pow
+
+open Real
+
+theorem Fa87 (theta p : ℝ) (h_theta : θ ≤ theta ∧ theta ≤ pi / 2) (h_p : θ ≤ p ∧ p ≤ 1):
+    exact
